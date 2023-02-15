9d84697b6eb3c4d98b13ce09616e211e6389afd2
Kaditcuy <philipukchimaobi@gmail.com>
2023-02-15 16:01:22 +0000

    Implemented 404 error functionality

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,6 +5,7 @@ from api.v1.views import app_views
 from flask import Flask
 from os import getenv
 from models import storage
+from flask import make_response
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -25,3 +26,7 @@ if __name__ == '__main__':
         port = 5000
 
     app.run(host=host, port=port, threaded=True)
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
