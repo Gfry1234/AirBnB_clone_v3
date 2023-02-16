c50a0bfbe430a1436da64841c1795a97b3280f0b
rotex <dantejake48@gmail.com>
2023-02-16 12:59:04 +0100

    fix

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -88,7 +88,7 @@ def update_place(place_id):
         abort(400, "Not a JSON")
 
     for key, val in data.items():
-        if key not in ['id', 'user_id', 'city_at', 'created_at', 'updated_at']:
+        if key not in ['id', 'user_id', 'city_id', 'created_at', 'updated_at']:
             setattr(place_inst, key, val)
 
     storage.save()
